394610a326ba8cd9424ee51775b6893e94f5e55f
Ivan Oliveira <79813537+IvanIndaia@users.noreply.github.com>
2022-07-12 23:10:32 -0300

    Up_Liberta

---
+++ html/javascript/javaScript.js
@@ -0,0 +1,31 @@
+function apostarLiberta() {
+
+    let cap1 = document.querySelector("#CAP-C")
+    let cap_c = parseInt(cap1.value)
+
+    let est1 = document.querySelector("#EST-F")
+    let est_f = parseInt(est1.value)
+
+    let est2 = document.querySelector("#EST-C")
+    let est_c = parseInt(est2.value)
+
+    let cap2 = document.querySelector("#CAP-F")
+    let cap_f = parseInt(cap2.value)
+
+    alert (`CAP ${cap_c} X ${est_f} EST \n EST ${est_c} X ${cap_f} CAP \n
+            
+            `)
+}
+
+// function apostarLiberta() {
+
+//     let cap_c = document.getElementById("#CAP-C").value
+//     let est_f = document.getElementById("#EST-F").value
+//     let est_c = document.getElementById("#EST-C").value
+//     let cap_f = document.getElementById("#CAP-F").value
+
+//     alert (`CAP ${cap_c} X ${est_f} EST \n 
+//             EST ${est_c} X ${cap_f} CAP \n
+            
+//             `)
+// }
