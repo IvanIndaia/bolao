31d1835cd78818d1bd800593ae03cee9ba94a81d
Ivan Oliveira <79813537+IvanIndaia@users.noreply.github.com>
2022-07-27 21:39:20 -0300

    Up.7

--- javascript/acao.js
+++ javascript/acao.js
@@ -175,7 +175,7 @@ function whatsapp() {
    let capf = parseInt(cap2.value)
 
   // ============= FIM =============
-  alert("NÃO SE ESQUECA DE FAZER O PIX: (19)992481225 \n Valor: R$ 5,00")
+  alert("NÃO SE ESQUECA DE FAZER O PIX: (19)992481225 \n Valor: R$ 7,00")
   let s = `APOSTA *FUTBR* - COPA DO BRASIL 2022%0A%0AACG ${acgc} X ${corf} COR%0ACOR ${corc} X ${acgf} ACG%0AFOR ${forc} X ${fluf} FLU%0AFLU ${fluc} X ${forf} FOR%0ASAO ${saoc} X ${amgf} AMG%0AAMG ${amgc} X ${saof} SAO%0ACAP ${capc} X ${flaf} FLA%0AFLA ${flac} X ${capf} CAP`
   // let s = toString(st)
 
@@ -186,7 +186,7 @@ function whatsapp() {
   // window.open(`https://api.whatsapp.com/send?phone=5519992481225&text=${s}`, "blank");
   // window.open(`https://web.whatsapp.com/send?phone=5519992481225&text=${s}`, "blank");
   // window.open(`https://web.whatsapp.com/send?phone=5519992481225&text=${s}`, "blank");
-  window.open(`https://wa.me/55019992481225&text=${s}`, "blank")
+  window.open(`https://wa.me/send?phone=55019992481225&text=${s}`)
 }
 
 function paginaBrasil() {
