229107e8cb9a8e55d250bb670e1bcf5763c49e17
Ivan Oliveira <79813537+IvanIndaia@users.noreply.github.com>
2022-10-26 13:14:52 -0300

    bolao-final-liberta

--- javascript/acaoLiberta.js
+++ javascript/acaoLiberta.js
@@ -1,62 +1,80 @@
 function apostarLibertadores() {
+
   // ============= EST x CAP =============
-  let est1 = document.querySelector("#EST-F")
-  let estf = parseInt(est1.value)
-  let cap1 = document.querySelector("#CAP-C")
-  let capc = parseInt(cap1.value)
+  let fla1 = document.querySelector("#FLA-C")
+  let fla = parseInt(fla1.value)
+  let cap1 = document.querySelector("#CAP-F")
+  let cap = parseInt(cap1.value)
 
-  let est2 = document.querySelector("#EST-C")
-  let estc = parseInt(est2.value)
-  let cap2 = document.querySelector("#CAP-F")
-  let capf = parseInt(cap2.value)
+  // // ============= EST x CAP =============
+  // let est1 = document.querySelector("#EST-F")
+  // let estf = parseInt(est1.value)
+  // let cap1 = document.querySelector("#CAP-C")
+  // let capc = parseInt(cap1.value)
 
-  // ============= PAL x CAM =============
+  // let est2 = document.querySelector("#EST-C")
+  // let estc = parseInt(est2.value)
+  // let cap2 = document.querySelector("#CAP-F")
+  // let capf = parseInt(cap2.value)
 
-  let cam1 = document.querySelector("#CAM-C")
-  let camc = parseInt(cam1.value)
-  let pal1 = document.querySelector("#PAL-F")
-  let palf = parseInt(pal1.value)
+  // // ============= PAL x CAM =============
 
-  let cam2 = document.querySelector("#CAM-F")
-  let camf = parseInt(cam2.value)
-  let pal2 = document.querySelector("#PAL-C")
-  let palc = parseInt(pal2.value)
+  // let cam1 = document.querySelector("#CAM-C")
+  // let camc = parseInt(cam1.value)
+  // let pal1 = document.querySelector("#PAL-F")
+  // let palf = parseInt(pal1.value)
 
-  // ============= FLA x COR =============
+  // let cam2 = document.querySelector("#CAM-F")
+  // let camf = parseInt(cam2.value)
+  // let pal2 = document.querySelector("#PAL-C")
+  // let palc = parseInt(pal2.value)
 
-  let fla1 = document.querySelector("#FLA-F")
-  let flaf = parseInt(fla1.value)
-  let cor1 = document.querySelector("#COR-C")
-  let corc = parseInt(cor1.value)
+  // // ============= FLA x COR =============
 
-  let fla2 = document.querySelector("#FLA-C")
-  let flac = parseInt(fla2.value)
-  let cor2 = document.querySelector("#COR-F")
-  let corf = parseInt(cor2.value)
+  // let fla1 = document.querySelector("#FLA-F")
+  // let flaf = parseInt(fla1.value)
+  // let cor1 = document.querySelector("#COR-C")
+  // let corc = parseInt(cor1.value)
 
-  // ============= TAL x VEL =============
+  // let fla2 = document.querySelector("#FLA-C")
+  // let flac = parseInt(fla2.value)
+  // let cor2 = document.querySelector("#COR-F")
+  // let corf = parseInt(cor2.value)
 
-  let vel2 = document.querySelector("#VEL-C")
-  let velc = parseInt(vel2.value)
-  let tal2 = document.querySelector("#TAL-F")
-  let talf = parseInt(tal2.value)
+  // // ============= TAL x VEL =============
 
-  let vel1 = document.querySelector("#VEL-F")
-  let velf = parseInt(vel1.value)
-  let tal1 = document.querySelector("#TAL-C")
-  let talc = parseInt(tal1.value)
+  // let vel2 = document.querySelector("#VEL-C")
+  // let velc = parseInt(vel2.value)
+  // let tal2 = document.querySelector("#TAL-F")
+  // let talf = parseInt(tal2.value)
 
-  // ============= FIM =============
+  // let vel1 = document.querySelector("#VEL-F")
+  // let velf = parseInt(vel1.value)
+  // let tal1 = document.querySelector("#TAL-C")
+  // let talc = parseInt(tal1.value)
+
+  // // ============= FIM =============
 
   let sou = document.querySelector("button")
   sou.setAttribute("data-target", "#meuModal")
 
   // let nome = prompt("Informe seu nome: ")
 
-  $(document).ready(function () {
-    $('#helloa').append(`<p style="text-align: center;"> CAP ${capc} X ${estf} EST <br> EST ${estc} X ${capf} CAP <br> CAM ${camc} X ${palf} PAL <br> PAL ${palc} X ${camf} CAM <br> COR ${corc} X ${flaf} FLA <br> FLA ${flac} X ${corf} COR <br> VEL ${velc} X ${talf} TAL <br> TAL ${talc} X ${velf} VEL <br> </p>`);
+  if (fla == cap) {
+    $(document).ready(function () {
+      $('#helloa').append(`<p style="text-align: center;"> FLA ${fla} X ${cap} CAP <br> Campeão: ${camp} </p>`);
+    });
+  } else {
+    $(document).ready(function () {
+      $('#helloa').append(`<p style="text-align: center;"> FLA ${fla} X ${cap} CAP <br> </p>`);
+    });
+  }
+  // $(document).ready(function () {
+  //   $('#helloa').append(`<p style="text-align: center;"> CAP ${capc} X ${estf} EST <br> EST ${estc} X ${capf} CAP <br> CAM ${camc} X ${palf} PAL <br> PAL ${palc} X ${camf} CAM <br> COR ${corc} X ${flaf} FLA <br> FLA ${flac} X ${corf} COR <br> VEL ${velc} X ${talf} TAL <br> TAL ${talc} X ${velf} VEL <br> </p>`);
 
-  });
+  // });
+
+
 
   let stg = $('.modal')
   stg.css({
@@ -65,125 +83,106 @@ function apostarLibertadores() {
 }
 
 function acionarModal() {
- // ============= EST x CAP =============
- let est1 = document.querySelector("#EST-F")
- let estf = parseInt(est1.value)
- let cap1 = document.querySelector("#CAP-C")
- let capc = parseInt(cap1.value)
 
- let est2 = document.querySelector("#EST-C")
- let estc = parseInt(est2.value)
- let cap2 = document.querySelector("#CAP-F")
- let capf = parseInt(cap2.value)
+  let fla1 = document.querySelector("#FLA-C")
+  let fla = parseInt(fla1.value)
 
- // ============= PAL x CAM =============
+  let cap1 = document.querySelector("#CAP-F")
+  let cap = parseInt(cap1.value)
 
- let cam1 = document.querySelector("#CAM-C")
- let camc = parseInt(cam1.value)
- let pal1 = document.querySelector("#PAL-F")
- let palf = parseInt(pal1.value)
+  //  // ============= EST x CAP =============
+  //  let est1 = document.querySelector("#EST-F")
+  //  let estf = parseInt(est1.value)
+  //  let cap1 = document.querySelector("#CAP-C")
+  //  let capc = parseInt(cap1.value)
 
- let cam2 = document.querySelector("#CAM-F")
- let camf = parseInt(cam2.value)
- let pal2 = document.querySelector("#PAL-C")
- let palc = parseInt(pal2.value)
+  //  let est2 = document.querySelector("#EST-C")
+  //  let estc = parseInt(est2.value)
+  //  let cap2 = document.querySelector("#CAP-F")
+  //  let capf = parseInt(cap2.value)
 
- // ============= FLA x COR =============
+  //  // ============= PAL x CAM =============
 
- let fla1 = document.querySelector("#FLA-F")
- let flaf = parseInt(fla1.value)
- let cor1 = document.querySelector("#COR-C")
- let corc = parseInt(cor1.value)
+  //  let cam1 = document.querySelector("#CAM-C")
+  //  let camc = parseInt(cam1.value)
+  //  let pal1 = document.querySelector("#PAL-F")
+  //  let palf = parseInt(pal1.value)
 
- let fla2 = document.querySelector("#FLA-C")
- let flac = parseInt(fla2.value)
- let cor2 = document.querySelector("#COR-F")
- let corf = parseInt(cor2.value)
+  //  let cam2 = document.querySelector("#CAM-F")
+  //  let camf = parseInt(cam2.value)
+  //  let pal2 = document.querySelector("#PAL-C")
+  //  let palc = parseInt(pal2.value)
 
- // ============= TAL x VEL =============
+  //  // ============= FLA x COR =============
 
- let vel2 = document.querySelector("#VEL-C")
- let velc = parseInt(vel2.value)
- let tal2 = document.querySelector("#TAL-F")
- let talf = parseInt(tal2.value)
+  //  let fla1 = document.querySelector("#FLA-F")
+  //  let flaf = parseInt(fla1.value)
+  //  let cor1 = document.querySelector("#COR-C")
+  //  let corc = parseInt(cor1.value)
 
- let vel1 = document.querySelector("#VEL-F")
- let velf = parseInt(vel1.value)
- let tal1 = document.querySelector("#TAL-C")
- let talc = parseInt(tal1.value)
+  //  let fla2 = document.querySelector("#FLA-C")
+  //  let flac = parseInt(fla2.value)
+  //  let cor2 = document.querySelector("#COR-F")
+  //  let corf = parseInt(cor2.value)
 
- // ============= FIM =============
+  //  // ============= TAL x VEL =============
+
+  //  let vel2 = document.querySelector("#VEL-C")
+  //  let velc = parseInt(vel2.value)
+  //  let tal2 = document.querySelector("#TAL-F")
+  //  let talf = parseInt(tal2.value)
+
+  //  let vel1 = document.querySelector("#VEL-F")
+  //  let velf = parseInt(vel1.value)
+  //  let tal1 = document.querySelector("#TAL-C")
+  //  let talc = parseInt(tal1.value)
 
   // ============= FIM =============
 
-  if (isNaN(capc) || isNaN(estf) || isNaN(estc) || isNaN(capf) ||
-    isNaN(camc) || isNaN(palf) || isNaN(palc) || isNaN(camf) ||
-    isNaN(corc) || isNaN(flaf) || isNaN(flac) || isNaN(corf) ||
-    isNaN(velc) || isNaN(talf) || isNaN(talc) || isNaN(velf)) {
-    alert("Favor preencher todos os resultados!")
+  // ============= FIM =============
+
+  if (isNaN(fla) || isNaN(cap)) {
+    alert("Favor Inserir um placar!")
+  } else if (fla == cap) {
+    camp = prompt("Quem será Campeão? ")
+    apostarLibertadores()
   } else {
     apostarLibertadores()
   }
+
+  // if (isNaN(capc) || isNaN(estf) || isNaN(estc) || isNaN(capf) ||
+  //   isNaN(camc) || isNaN(palf) || isNaN(palc) || isNaN(camf) ||
+  //   isNaN(corc) || isNaN(flaf) || isNaN(flac) || isNaN(corf) ||
+  //   isNaN(velc) || isNaN(talf) || isNaN(talc) || isNaN(velf)) {
+  //   alert("Favor preencher todos os resultados!")
+  // } else {
+  //   apostarLibertadores()
+  // }
 }
 
 
 function whatsapp() {
   // $(location).attr('href', "https://wa.me/55019992481225")
   // ============= EST x CAP =============
-  let est1 = document.querySelector("#EST-F")
-  let estf = parseInt(est1.value)
-  let cap1 = document.querySelector("#CAP-C")
-  let capc = parseInt(cap1.value)
+  let fla1 = document.querySelector("#FLA-C")
+  let fla = parseInt(fla1.value)
+  let cap1 = document.querySelector("#CAP-F")
+  let cap = parseInt(cap1.value)
 
-  let est2 = document.querySelector("#EST-C")
-  let estc = parseInt(est2.value)
-  let cap2 = document.querySelector("#CAP-F")
-  let capf = parseInt(cap2.value)
-
-  // ============= PAL x CAM =============
-
-  let cam1 = document.querySelector("#CAM-C")
-  let camc = parseInt(cam1.value)
-  let pal1 = document.querySelector("#PAL-F")
-  let palf = parseInt(pal1.value)
-
-  let cam2 = document.querySelector("#CAM-F")
-  let camf = parseInt(cam2.value)
-  let pal2 = document.querySelector("#PAL-C")
-  let palc = parseInt(pal2.value)
-
-  // ============= FLA x COR =============
-
-  let fla1 = document.querySelector("#FLA-F")
-  let flaf = parseInt(fla1.value)
-  let cor1 = document.querySelector("#COR-C")
-  let corc = parseInt(cor1.value)
-
-  let fla2 = document.querySelector("#FLA-C")
-  let flac = parseInt(fla2.value)
-  let cor2 = document.querySelector("#COR-F")
-  let corf = parseInt(cor2.value)
-
-  // ============= TAL x VEL =============
-
-  let vel2 = document.querySelector("#VEL-C")
-  let velc = parseInt(vel2.value)
-  let tal2 = document.querySelector("#TAL-F")
-  let talf = parseInt(tal2.value)
-
-  let vel1 = document.querySelector("#VEL-F")
-  let velf = parseInt(vel1.value)
-  let tal1 = document.querySelector("#TAL-C")
-  let talc = parseInt(tal1.value)
-
+  // alert(`O Campeão será: ${camp}`)
   // ============= FIM =============
-  
+
+  if (fla == cap) {
+    alert("NÃO SE ESQUECA DE FAZER O PIX: (19)992481225 \nValor: R$ 5,00")
+    let text = `APOSTA *FUTBR* - FINAL DA COPA LIBERTADORES 2022%0A%0AFLA ${fla} X ${cap} CAP%0A Campeão: ${camp}`
+    window.open(`https://wa.me/5519992481225?text=${text}`)
+  }
   alert("NÃO SE ESQUECA DE FAZER O PIX: (19)992481225 \nValor: R$ 5,00")
-  let s = `APOSTA *FUTBR* - COPA LIBERTADORES 2022%0A%0ACAP ${capc} X ${estf} EST%0AEST ${estc} X ${capf} CAP%0ACAM ${camc} X ${palf} PAL%0APAL ${palc} X ${camf} CAM%0ACOR ${corc} X ${flaf} FLA%0AFLA ${flac} X ${corf} COR%0AVEL ${velc} X ${talf} TAL%0ATAL ${talc} X ${velf} VEL`
+  let text = `APOSTA *FUTBR* - FINAL DA COPA LIBERTADORES 2022%0A%0AFLA ${fla} X ${cap} CAP`
   // window.open(`https://api.whatsapp.com/send?phone=5519992481225&text=${s}`, "blank");
   // window.open(`https://web.whatsapp.com/send?phone=5519992481225&text=${s}`, "blank");
   // window.open(`https://web.whatsapp.com/send?phone=5519992481225&text=${s}`, "blank");
-  window.open(`https://wa.me/5519992481225?text=${s}`)
+  window.open(`https://wa.me/5519992481225?text=${text}`)
 }
 
 function paginaLiberta() {
