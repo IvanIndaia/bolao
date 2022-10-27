315ce051bacf24055ac2f16b20e2a09676a5fef9
Ivan Oliveira <79813537+IvanIndaia@users.noreply.github.com>
2022-10-27 17:35:48 -0300

    Bolão-Almost

--- javascript/acaoLiberta.js
+++ javascript/acaoLiberta.js
@@ -1,3 +1,12 @@
+function empateLiberta() {
+  let stg = $('#meuModalB')
+  stg.css({
+    "visibility": "visible"
+  })
+}
+/* ================================================ */
+/* ============= APOSTAR LIBERTADORES ============= */
+/* ================================================ */
 function apostarLibertadores() {
 
   // ============= EST x CAP =============
@@ -6,59 +15,10 @@ function apostarLibertadores() {
   let cap1 = document.querySelector("#CAP-F")
   let cap = parseInt(cap1.value)
 
-  // // ============= EST x CAP =============
-  // let est1 = document.querySelector("#EST-F")
-  // let estf = parseInt(est1.value)
-  // let cap1 = document.querySelector("#CAP-C")
-  // let capc = parseInt(cap1.value)
 
-  // let est2 = document.querySelector("#EST-C")
-  // let estc = parseInt(est2.value)
-  // let cap2 = document.querySelector("#CAP-F")
-  // let capf = parseInt(cap2.value)
-
-  // // ============= PAL x CAM =============
-
-  // let cam1 = document.querySelector("#CAM-C")
-  // let camc = parseInt(cam1.value)
-  // let pal1 = document.querySelector("#PAL-F")
-  // let palf = parseInt(pal1.value)
-
-  // let cam2 = document.querySelector("#CAM-F")
-  // let camf = parseInt(cam2.value)
-  // let pal2 = document.querySelector("#PAL-C")
-  // let palc = parseInt(pal2.value)
-
-  // // ============= FLA x COR =============
-
-  // let fla1 = document.querySelector("#FLA-F")
-  // let flaf = parseInt(fla1.value)
-  // let cor1 = document.querySelector("#COR-C")
-  // let corc = parseInt(cor1.value)
-
-  // let fla2 = document.querySelector("#FLA-C")
-  // let flac = parseInt(fla2.value)
-  // let cor2 = document.querySelector("#COR-F")
-  // let corf = parseInt(cor2.value)
-
-  // // ============= TAL x VEL =============
-
-  // let vel2 = document.querySelector("#VEL-C")
-  // let velc = parseInt(vel2.value)
-  // let tal2 = document.querySelector("#TAL-F")
-  // let talf = parseInt(tal2.value)
-
-  // let vel1 = document.querySelector("#VEL-F")
-  // let velf = parseInt(vel1.value)
-  // let tal1 = document.querySelector("#TAL-C")
-  // let talc = parseInt(tal1.value)
-
-  // // ============= FIM =============
 
   let sou = document.querySelector("button")
   sou.setAttribute("data-target", "#meuModal")
-
-  // let nome = prompt("Informe seu nome: ")
 
   if (fla == cap) {
     $(document).ready(function () {
@@ -69,19 +29,63 @@ function apostarLibertadores() {
       $('#helloa').append(`<p style="text-align: center;"> FLA ${fla} X ${cap} CAP <br> </p>`);
     });
   }
-  // $(document).ready(function () {
-  //   $('#helloa').append(`<p style="text-align: center;"> CAP ${capc} X ${estf} EST <br> EST ${estc} X ${capf} CAP <br> CAM ${camc} X ${palf} PAL <br> PAL ${palc} X ${camf} CAM <br> COR ${corc} X ${flaf} FLA <br> FLA ${flac} X ${corf} COR <br> VEL ${velc} X ${talf} TAL <br> TAL ${talc} X ${velf} VEL <br> </p>`);
 
-  // });
-
-
-
-  let stg = $('.modal')
+  let stg = $('#meuModalA')
   stg.css({
     "visibility": "visible"
   })
 }
 
+
+/* ================================================ */
+/* ==================== POP-UP'S  ================= */
+/* ================================================ */
+// var x = document.querySelectorAll('input')
+function habilitarBotao() {
+
+  var x = document.querySelectorAll('input')
+
+  for (let i = 0; i < 5; i++) {
+    if (x[i].checked) {
+      console.log(`Flegado`)
+      let stg = $('#botaoConf')
+      stg.css({
+        "visibility": "visible"
+      })
+
+      // console.log(`Flegado: ${x[i].value}`)
+      // camp = x[i].value
+    }
+  }
+}
+
+
+/* ================================================ */
+/* =============== CAPTURAR CAMPEÃO =============== */
+/* ================================================ */
+function capturaCampeao() {
+
+  var x = document.querySelectorAll('input')
+
+  for (let i = 0; i < 5; i++) {
+    if (x[i].checked) {
+      console.log(`Flegado: ${x[i].value}`)
+      camp = x[i].value
+    }
+  }
+
+  let stg = $('#meuModalB')
+  stg.css({
+    "visibility": "hidden"
+  })
+  apostarLibertadores()
+}
+
+
+
+/* ================================================ */
+/* ================ ACIONAR MODAL ================= */
+/* ================================================ */
 function acionarModal() {
 
   let fla1 = document.querySelector("#FLA-C")
@@ -90,77 +94,24 @@ function acionarModal() {
   let cap1 = document.querySelector("#CAP-F")
   let cap = parseInt(cap1.value)
 
-  //  // ============= EST x CAP =============
-  //  let est1 = document.querySelector("#EST-F")
-  //  let estf = parseInt(est1.value)
-  //  let cap1 = document.querySelector("#CAP-C")
-  //  let capc = parseInt(cap1.value)
 
-  //  let est2 = document.querySelector("#EST-C")
-  //  let estc = parseInt(est2.value)
-  //  let cap2 = document.querySelector("#CAP-F")
-  //  let capf = parseInt(cap2.value)
-
-  //  // ============= PAL x CAM =============
-
-  //  let cam1 = document.querySelector("#CAM-C")
-  //  let camc = parseInt(cam1.value)
-  //  let pal1 = document.querySelector("#PAL-F")
-  //  let palf = parseInt(pal1.value)
-
-  //  let cam2 = document.querySelector("#CAM-F")
-  //  let camf = parseInt(cam2.value)
-  //  let pal2 = document.querySelector("#PAL-C")
-  //  let palc = parseInt(pal2.value)
-
-  //  // ============= FLA x COR =============
-
-  //  let fla1 = document.querySelector("#FLA-F")
-  //  let flaf = parseInt(fla1.value)
-  //  let cor1 = document.querySelector("#COR-C")
-  //  let corc = parseInt(cor1.value)
-
-  //  let fla2 = document.querySelector("#FLA-C")
-  //  let flac = parseInt(fla2.value)
-  //  let cor2 = document.querySelector("#COR-F")
-  //  let corf = parseInt(cor2.value)
-
-  //  // ============= TAL x VEL =============
-
-  //  let vel2 = document.querySelector("#VEL-C")
-  //  let velc = parseInt(vel2.value)
-  //  let tal2 = document.querySelector("#TAL-F")
-  //  let talf = parseInt(tal2.value)
-
-  //  let vel1 = document.querySelector("#VEL-F")
-  //  let velf = parseInt(vel1.value)
-  //  let tal1 = document.querySelector("#TAL-C")
-  //  let talc = parseInt(tal1.value)
-
-  // ============= FIM =============
-
-  // ============= FIM =============
 
   if (isNaN(fla) || isNaN(cap)) {
-    alert("Favor Inserir um placar!")
+    alert("Favor Inserir um placar válido!")
   } else if (fla == cap) {
-    camp = prompt("Quem será Campeão? ")
-    apostarLibertadores()
+    empateLiberta()
+    // apostarLibertadores()
+
   } else {
     apostarLibertadores()
   }
-
-  // if (isNaN(capc) || isNaN(estf) || isNaN(estc) || isNaN(capf) ||
-  //   isNaN(camc) || isNaN(palf) || isNaN(palc) || isNaN(camf) ||
-  //   isNaN(corc) || isNaN(flaf) || isNaN(flac) || isNaN(corf) ||
-  //   isNaN(velc) || isNaN(talf) || isNaN(talc) || isNaN(velf)) {
-  //   alert("Favor preencher todos os resultados!")
-  // } else {
-  //   apostarLibertadores()
-  // }
 }
 
 
+
+/* ================================================ */
+/* ==================== WHATSAPP ================== */
+/* ================================================ */
 function whatsapp() {
   // $(location).attr('href', "https://wa.me/55019992481225")
   // ============= EST x CAP =============
@@ -176,19 +127,27 @@ function whatsapp() {
     alert("NÃO SE ESQUECA DE FAZER O PIX: (19)992481225 \nValor: R$ 5,00")
     let text = `APOSTA *FUTBR* - FINAL DA COPA LIBERTADORES 2022%0A%0A*FLA* ${fla} X ${cap} *CAP*%0A *Campeão:* ${camp}`
     window.open(`https://wa.me/5519992481225?text=${text}`)
+    paginaLiberta()
   }
   alert("NÃO SE ESQUECA DE FAZER O PIX: (19)992481225 \nValor: R$ 5,00")
   let text = `APOSTA *FUTBR* - FINAL DA COPA LIBERTADORES 2022%0A%0A*FLA* ${fla} X ${cap} *CAP*`
-  // window.open(`https://api.whatsapp.com/send?phone=5519992481225&text=${s}`, "blank");
-  // window.open(`https://web.whatsapp.com/send?phone=5519992481225&text=${s}`, "blank");
-  // window.open(`https://web.whatsapp.com/send?phone=5519992481225&text=${s}`, "blank");
+
   window.open(`https://wa.me/5519992481225?text=${text}`)
+  paginaLiberta()
 }
 
+
+/* ================================================ */
+/* ================= PÁGINA LIBERTA =============== */
+/* ================================================ */
 function paginaLiberta() {
   $(location).attr('href', '../html/libertadores.html')
 }
 
+
+/* ================================================ */
+/* ==================== POP-UP'S  ================= */
+/* ================================================ */
 function popUP() {
   alert("Em Breve...")
 }
