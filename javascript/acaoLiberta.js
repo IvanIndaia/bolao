5dfdbaa2ebc32319d2a387ed663eda8c84c5bd31
Ivan Oliveira <79813537+IvanIndaia@users.noreply.github.com>
2022-10-28 15:37:04 -0300

    ajuste-table

--- javascript/acaoLiberta.js
+++ javascript/acaoLiberta.js
@@ -135,7 +135,7 @@ function whatsapp() {
     window.open(`https://wa.me/5519992481225?text=${text}`)
     paginaLiberta()
   }
-  alert("NÃO SE ESQUECA DE FAZER O PIX: (19)992481225 \nValor: R$ 5,00")
+  alert("NÃO SE ESQUECA DE FAZER O PIX: (19)992481225 \nValor: R$ 5,00\nA msg precisa ser enviada para o Whatsapp para validação.")
   let text = `APOSTA *FUTBR* - FINAL DA COPA LIBERTADORES 2022%0A%0A*FLA* ${fla} X ${cap} *CAP*%0APix: 19992481225`
 
   window.open(`https://wa.me/5519992481225?text=${text}`)
