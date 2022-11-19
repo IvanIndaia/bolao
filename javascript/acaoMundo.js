6e1015264a6c0743f8f03705c92daec18c4fc2a8
IvanIndaia <ivan.indaia@gmail.com>
2022-11-19 13:55:22 -0300

    keep-CopaDoMundo_004

--- javascript/acaoMundo.js
+++ javascript/acaoMundo.js
@@ -1,178 +1,24 @@
-function apostarCopaBrasil() {
-  // ============= COR x ACG =============
-  // let cor1 = document.querySelector("#COR-F")
-  // let corf = parseInt(cor1.value)
-  // let acg1 = document.querySelector("#ACG-C")
-  // let acgc = parseInt(acg1.value)
-
-  let cor2 = document.querySelector("#COR-C")
-  let corc = parseInt(cor2.value)
-  let acg2 = document.querySelector("#ACG-F")
-  let acgf = parseInt(acg2.value)
-
-  // ============= FLU x FOR =============
-
-  let flu1 = document.querySelector("#FLU-C")
-  let fluc = parseInt(flu1.value)
-  let for1 = document.querySelector("#FOR-F")
-  let forf = parseInt(for1.value)
-
-  let flu2 = document.querySelector("#FLU-F")
-  let fluf = parseInt(flu2.value)
-  let for2 = document.querySelector("#FOR-C")
-  let forc = parseInt(for2.value)
-
-  // ============= AMG x SAO =============
-
-  let amg1 = document.querySelector("#AMG-F")
-  let amgf = parseInt(amg1.value)
-  let sao1 = document.querySelector("#SAO-C")
-  let saoc = parseInt(sao1.value)
-
-  let amg2 = document.querySelector("#AMG-C")
-  let amgc = parseInt(amg2.value)
-  let sao2 = document.querySelector("#SAO-F")
-  let saof = parseInt(sao2.value)
-
-  // ============= FLA x CAP =============
-
-  // let fla2 = document.querySelector("#FLA-C")
-  // let flac = parseInt(fla2.value)
-  // let cap2 = document.querySelector("#CAP-F")
-  // let capf = parseInt(cap2.value)
-
-  let fla1 = document.querySelector("#FLA-F")
-  let flaf = parseInt(fla1.value)
-  let cap1 = document.querySelector("#CAP-C")
-  let capc = parseInt(cap1.value)
-
-  // ============= FIM =============
-
-  let sou = document.querySelector("button")
-  sou.setAttribute("data-target", "#meuModal")
-
-  // let nome = prompt("Informe seu nome: ")
-
-  $(document).ready(function () {
-    $('#helloa').append(`<p style="text-align: center;"> ACG 2 X 0 COR <br> COR ${corc} X ${acgf} ACG <br> FOR ${forc} X ${fluf} FLU <br> FLU ${fluc} X ${forf} FOR <br> SAO ${saoc} X ${amgf} AMG <br> AMG ${amgc} X ${saof} SAO <br> FLA 0 X 0 CAP <br> CAP ${capc} X ${flaf} FLA <br> </p>`);
-
-  });
-
-  let stg = $('.modal')
-  stg.css({
-    "visibility": "visible"
-  })
-}
 
 function acionarModal() {
-  // ============= COR x ACG =============
-  //  let cor1 = document.querySelector("#COR-F")
-  //  let corf = parseInt(cor1.value)
-  //  let acg1 = document.querySelector("#ACG-C")
-  //  let acgc = parseInt(acg1.value)
 
-  let cor2 = document.querySelector("#COR-C")
-  let corc = parseInt(cor2.value)
-  let acg2 = document.querySelector("#ACG-F")
-  let acgf = parseInt(acg2.value)
+  let fla1 = document.querySelector("#FLA-C")
+  let fla = parseInt(fla1.value)
 
-  // ============= FLU x FOR =============
+  let cap1 = document.querySelector("#CAP-F")
+  let cap = parseInt(cap1.value)
 
-  let flu1 = document.querySelector("#FLU-C")
-  let fluc = parseInt(flu1.value)
-  let for1 = document.querySelector("#FOR-F")
-  let forf = parseInt(for1.value)
 
-  let flu2 = document.querySelector("#FLU-F")
-  let fluf = parseInt(flu2.value)
-  let for2 = document.querySelector("#FOR-C")
-  let forc = parseInt(for2.value)
 
-  // ============= AMG x SAO =============
+  if (isNaN(fla) || isNaN(cap)) {
+    alert("Favor Inserir um placar válido!")
+  } else if (fla == cap) {
+    empateLiberta()
+    // apostarLibertadores()
 
-  let amg1 = document.querySelector("#AMG-F")
-  let amgf = parseInt(amg1.value)
-  let sao1 = document.querySelector("#SAO-C")
-  let saoc = parseInt(sao1.value)
-
-  let amg2 = document.querySelector("#AMG-C")
-  let amgc = parseInt(amg2.value)
-  let sao2 = document.querySelector("#SAO-F")
-  let saof = parseInt(sao2.value)
-
-  // ============= FLA x CAP =============
-
-  // let fla1 = document.querySelector("#FLA-F")
-  // let flaf = parseInt(fla1.value)
-  // let cap1 = document.querySelector("#CAP-C")
-  // let capc = parseInt(cap1.value)
-
-  let fla2 = document.querySelector("#FLA-C")
-  let flac = parseInt(fla2.value)
-  let cap2 = document.querySelector("#CAP-F")
-  let capf = parseInt(cap2.value)
-
-  // ============= FIM =============
-
-  if (isNaN(corc) || isNaN(acgf) ||
-    isNaN(forc) || isNaN(fluf) || isNaN(fluc) || isNaN(forf) ||
-    isNaN(saoc) || isNaN(amgf) || isNaN(amgc) || isNaN(saof) ||
-    isNaN(capc) || isNaN(flaf)) {
-    alert("Favor preencher todos os resultados!")
   } else {
-    apostarCopaBrasil()
+    apostarLibertadores()
   }
 }
-
-
-function whatsapp() {
-  // $(location).attr('href', "https://wa.me/55019992481225")
-  // ============= COR x ACG =============
-  // let cor1 = document.querySelector("#COR-F")
-  // let corf = parseInt(cor1.value)
-  // let acg1 = document.querySelector("#ACG-C")
-  // let acgc = parseInt(acg1.value)
-
-  let cor2 = document.querySelector("#COR-C")
-  let corc = parseInt(cor2.value)
-  let acg2 = document.querySelector("#ACG-F")
-  let acgf = parseInt(acg2.value)
-
-  // ============= FLU x FOR =============
-
-  let flu1 = document.querySelector("#FLU-C")
-  let fluc = parseInt(flu1.value)
-  let for1 = document.querySelector("#FOR-F")
-  let forf = parseInt(for1.value)
-
-  let flu2 = document.querySelector("#FLU-F")
-  let fluf = parseInt(flu2.value)
-  let for2 = document.querySelector("#FOR-C")
-  let forc = parseInt(for2.value)
-
-  // ============= AMG x SAO =============
-
-  let amg1 = document.querySelector("#AMG-F")
-  let amgf = parseInt(amg1.value)
-  let sao1 = document.querySelector("#SAO-C")
-  let saoc = parseInt(sao1.value)
-
-  let amg2 = document.querySelector("#AMG-C")
-  let amgc = parseInt(amg2.value)
-  let sao2 = document.querySelector("#SAO-F")
-  let saof = parseInt(sao2.value)
-
-  // ============= FLA x CAP =============
-
-  //  let fla2 = document.querySelector("#FLA-C")
-  //  let flac = parseInt(fla2.value)
-  //  let cap2 = document.querySelector("#CAP-F")
-  //  let capf = parseInt(cap2.value)
-
-  let fla1 = document.querySelector("#FLA-F")
-  let flaf = parseInt(fla1.value)
-  let cap1 = document.querySelector("#CAP-C")
-  let capc = parseInt(cap1.value)
 
   // ================= FIM =================
 
